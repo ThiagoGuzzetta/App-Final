0a728617640e3eb419633651efaaa7d8f5aacf5e
ThiagoGuzzetta <thiagoguzzetta@gmail.com>
2025-06-17 11:52:37 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'inventario-cache-v1';
+const urlsToCache = [
+  '.',
+  './',
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
